5965bacfc5bbe9c8c8db965f2b73d66e19a0b45f
Kavya Bhardwaj <kbspace2004@gmail.com>
2026-09-24 22:36:00 +0530

    fix: remove unused ClerkProvider to fix CSP errors

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -9,10 +9,8 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || "";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ClerkProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
